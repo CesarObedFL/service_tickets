5b753ed934e9235d555bd6eced6bedaa23f5e834
CesarObedFL <cesarobedfl@gmail.com>
2023-01-06 17:44:22 -0600

    [ADD] graphs parameters in the dashboard

--- app/Http/Livewire/Dashboard.php
+++ app/Http/Livewire/Dashboard.php
@@ -16,6 +16,14 @@ class Dashboard extends Component
         'in_process_tickets' => 0, 'in_process_tickets_percent' => 0
     ];
 
+    public $today_tickets = [ 0, 0, 0, 0 ]; // total, solved, pending, in_process
+
+    public $month_days = 0;
+    public $month_days_labels = [];
+    public $month_tickets = [];
+    public $month_solved_tickets = [];
+    public $month_pending_tickets = [];
+
     // calculates the variables
     public function mount()
     {
@@ -23,6 +31,19 @@ class Dashboard extends Component
         $this->ticket_statistics['solved_tickets'] = Ticket::where('status', 'solved')->count();
         $this->ticket_statistics['pending_tickets'] = Ticket::where('status', 'pending')->count();
         $this->ticket_statistics['in_process_tickets'] = Ticket::where('status', 'in process')->count();
+
+        $this->today_tickets[0] = Ticket::whereDate('created_at', date('y-m-d'))->count();
+        $this->today_tickets[1] = Ticket::whereDate('created_at', date('y-m-d'))->where('status', 'solved')->count();
+        $this->today_tickets[2] = Ticket::whereDate('created_at', date('y-m-d'))->where('status', 'pending')->count();
+        $this->today_tickets[3] = Ticket::whereDate('created_at', date('y-m-d'))->where('status', 'in process')->count();
+
+        $this->month_days = cal_days_in_month(CAL_GREGORIAN, date('m'), date('y'));
+        for ($i = 0; $i < $this->month_days; $i++) {
+            $this->month_days_labels[$i] = (string)($i+1).'th';
+            $this->month_tickets[$i] = Ticket::whereMonth('created_at', date('m'))->whereDay('created_at', ($i+1))->count();
+            $this->month_solved_tickets[$i] = Ticket::whereMonth('created_at', date('m'))->whereDay('created_at', ($i+1))->where('status', 'solved')->count();
+            $this->month_pending_tickets[$i] = Ticket::whereMonth('created_at', date('m'))->whereDay('created_at', ($i+1))->where('status', 'pending')->count();
+        }
 
         $this->ticket_statistics['solved_tickets_percent'] = ($this->ticket_statistics['solved_tickets'] * 100) /  (( $this->ticket_statistics['total_tickets'] > 0 ) ? $this->ticket_statistics['total_tickets'] : 1);
         $this->ticket_statistics['pending_tickets_percent'] = ($this->ticket_statistics['pending_tickets'] * 100) / (( $this->ticket_statistics['total_tickets'] > 0 ) ? $this->ticket_statistics['total_tickets'] : 1);
